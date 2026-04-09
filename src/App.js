99658d80928bf63943532efa770200c0af10bf4e
Saniya-00 <2400033301@kluniversity.in>
2026-04-09 12:23:53 +0530

    final skill13 proje t

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/students")
+      .then(res => res.json())
+      .then(data => setStudents(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <h1>Student List</h1>
+
+      <table border="1" align="center">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Course</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {students.map(s => (
+            <tr key={s.id}>
+              <td>{s.id}</td>
+              <td>{s.name}</td>
+              <td>{s.email}</td>
+              <td>{s.course}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
